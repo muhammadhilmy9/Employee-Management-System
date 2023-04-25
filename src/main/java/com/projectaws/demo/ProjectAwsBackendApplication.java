080b17fd657f3655679a9a2b386d510d8ecb3b9c
muhammadhilmya.st <muhammadhilmya.st@gmail.com>
2023-04-25 17:41:05 +0700

    commit

---
+++ src/main/java/com/projectaws/demo/ProjectAwsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projectaws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAwsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAwsBackendApplication.class, args);
+	}
+
+}
